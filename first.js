15ee6b2fe13d1f5ac1e9bd4e8c0c0cc3bb2f3f72
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-09-19 16:35:12 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,32 @@
+// console.log("This is js file")
+// alert("warning")
+// document.write("hello friends")
+
+
+// var num = 12
+// var num = 90
+
+// console.log(num)     // be redeclare and reassign
+
+
+// let n1 = 22
+// n1 = "redhat1"
+
+// console.log(n1)      // cant be redeclare but can be reassign
+
+
+// const name = "priyesh"
+
+// console.log()
+
+
+
+
+
+
+num1 = parseInt(prompt("enter no 1"))
+num2 = parseInt(prompt("enter no 2"))               //it changes string function into integer 
+
+console.log(num1+num2)
+
+//    H.W  (operators)
