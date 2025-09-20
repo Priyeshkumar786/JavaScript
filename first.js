e3120c4bfafb7d6f20b1caa16aef03b1d3260ab4
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-09-20 11:51:52 +0530

    first operation

--- first.js
+++ first.js
@@ -24,9 +24,18 @@
 
 
 
-num1 = parseInt(prompt("enter no 1"))
-num2 = parseInt(prompt("enter no 2"))               //it changes string function into integer 
+// num1 = parseInt(prompt("enter no 1"))
+// num2 = parseInt(prompt("enter no 2"))               //it changes string function into integer 
 
-console.log(num1+num2)
+// console.log(num1+num2)
 
 //    H.W  (operators)
+
+
+num1= parseInt(prompt("what is first number"))
+num2= parseInt(prompt("what is second number"))
+
+console.log(num1+num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
