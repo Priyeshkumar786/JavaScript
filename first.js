4e431733fb3e05c0fb08b364a5da8821cb96dcc6
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-09-20 18:03:24 +0530

    todays topics

--- first.js
+++ first.js
@@ -32,13 +32,70 @@
 //    H.W  (operators)
 
 
-num1= parseInt(prompt("what is first number"))
-num2= parseInt(prompt("what is second number"))
+// num1= parseInt(prompt("what is first number"))
+// num2= parseInt(prompt("what is second number"))
 
-console.log(num1+num2)
-console.log(num1*num2)
-console.log(num1/num2)
-console.log(num1%num2)
+// console.log(num1+num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
 
-document.write("hello this is priyesh")
-alert("Something went wrong")
+// document.write("hello this is priyesh")
+// alert("Something went wrong")
+
+
+
+// write a program area of circle radius from user
+
+// let radius= parseInt(prompt("enter radius value"))
+
+// console.log(3.14*radius*radius)
+
+
+// let x = 90
+// let y = 8
+// let z 
+
+// x=z
+// x=y
+// z=y
+
+// console.log(x,z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// write a program integer inp from user find remainder of that number number shoul be devisible by 2
+// let user=parseInt(prompt("enter number"))
+
+
+// console.log(user%2)
+
+
+let l=parseInt(prompt("enter length"))
+let b=parseInt(prompt("enter breadth"))
+
+
+console.log(l*b)
