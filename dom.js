5f7fb9f8f879cb0207786022dfa8cf7d91b4985b
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-10-07 16:29:44 +0530

    added todays topic

--- dom.js
+++ dom.js
@@ -1,35 +1,95 @@
-document.getElementById('head1').textContent = "from js 1"
-document.getElementById('head2').textContent = "from js 2"
-document.getElementById('head3').textContent = "from js 3"
-document.getElementById('head4').textContent = "from js 4"
-document.getElementById('head5').textContent = "from js 5"
-document.getElementById('head6').textContent = "from js 6"
+// document.getElementById('head1').textContent = "from js 1"
+// document.getElementById('head2').textContent = "from js 2"
+// document.getElementById('head3').textContent = "from js 3"
+// document.getElementById('head4').textContent = "from js 4"
+// document.getElementById('head5').textContent = "from js 5"
+// document.getElementById('head6').textContent = "from js 6"
 
-// we store in a variable through whic h we can use it multiple times
+// // we store in a variable through which we can use it multiple times
 
-let a = document.getElementById('demo')
-a.textContent ="another way"
+// let a = document.getElementById('demo')
+// a.textContent ="another way"
 
 
-// innerhtml command can change text as well as inner html also.
+// // innerhtml command can change text as well as inner html also.
 
-let b = document.getElementById('LILY')
-b.innerHTML = "<i> LILY BHAI </i>"              // if we have to change only a single element we putting innerhtml
-b.setAttribute('align','center')                // we can directly change body by applying directly set attribute
+// let b = document.getElementById('LILY')
+// b.innerHTML = "<i> LILY BHAI </i>"              // if we have to change only a single element we putting innerhtml
+// b.setAttribute('align','center')                // we can directly change body by applying directly set attribute
 
-let c =document.getElementById('line')
-c.innerHTML = "<u>this is priyesh</u>"
+// let c =document.getElementById('line')
+// c.innerHTML = "<u>this is priyesh</u>"
 
     
-//              (function) (att.name)(att.value)
-document.body.setAttribute('bgcolor','yellow')
+// //            (function) (att.name)      (att.value)
+// document.body.setAttribute('bgcolor',    'yellow')
 
-document.getElementById('head').removeAttribute('style')   // we will remove any css and styling through javascript.
+// document.getElementById('head').removeAttribute('style')   // we will remove any css and styling through javascript.
 
 
-added additional funtion 
-added more
-more functions
-again another functions
-again
-done expression related to arrow functions
+// added additional funtion 
+// added more
+// more functions
+// again another functions
+// again
+// done expression related to arrow functions
+
+// document.getElementById('image')
+// setAttribute('src','image')
+
+
+// ========================================================================
+
+//if user want to add single property  in inner html we have to give one css as in single line with the help of js
+
+// document.getElementById('head').style.backgroundColor="yellow"
+
+// let head= document.getElementById('demo')
+// head.style.backgroundColor="lightblue"
+// head.style.padding="20px"
+
+
+// let one=document.getElementById('cybrom')
+// one.style.backgroundColor="red"
+// one.style.padding="15px"
+// one.style.textAlign="center"
+
+// let two= document.getElementById('para')
+// two.style.backgroundColor="lightgreen"
+// two.style.padding="15px"
+// two.style.textAlign="center"
+
+// let three= document.getElementById('button')
+// three.style.backgroundColor="blue"
+// three.style.padding="5px"
+// three.style.textAlign="center"
+
+
+
+// let head1 = document.getElementById('chair')
+// head1.style.cssText = "background-color:'red;color:white;padding:20px"
+
+
+// element
+
+// let tag = document.createElement('h1')
+
+// tag.textContent = "new element from js file"
+// console.log(tag);
+
+// document.getElementById('parent').append(tag)
+
+
+// ===========================
+
+
+let joy = document.createElement('p')
+
+joy.textContent = "new element from js file"
+console.log(joy);
+
+document.getElementById('new').append(joy)
+
+// ===========================================================H W================================================================
+
+// have to put 10 items put green , padding 12px ,text white in every even numbers of items
