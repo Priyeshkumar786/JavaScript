be8a0213904737dbb0d3ad3997d28cbbecf20731
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-10-10 16:24:54 +0530

    added todays topic of array

--- homework.js
+++ homework.js
@@ -73,37 +73,105 @@
 // -------------------------------------------------------09/10/2025-------------------------------------------------------------
 
 
- function show(device) {
-      let img = document.getElementById("deviceImage");
+//  function show(device) {
+//       let img = document.getElementById("deviceImage");
 
-      if (device === "mobile") {
-        img.src = "mobile.jpeg";
-      } else if (device === "tablet") {
-        img.src = "tablet.jpg"
-      } else if (device === "laptop") {
-        img.src = "laptop.jpeg";
-      }
+//       if (device == "mobile") {
+//         img.src = "mobile.jpeg"
+//       } else if (device == "tablet") {
+//         img.src = "tablet.jpg"
+//       } else if (device == "laptop") {
+//         img.src = "laptop.jpeg"
+//       }
 
-      img.alt = device + " image";
-    }
+//       img.alt = device + " image"
+//     }
 
-    document.getElementById("deviceImage").style.height="250px"
-
-
-    // -------------------------------------------------------------
+//     document.getElementById("deviceImage").style.height="250px"
 
 
+    // ------------------------------------------by sir-------------------
 
-    function showFlower(flower) {
-      let img = document.getElementById("flowerImage");
+    // function fun1(){
+    //   document.getElementById('image').src="mobile.jpeg"
+    // }
 
-      if (flower === "rose") {
-        img.src = "https://cdn.pixabay.com/photo/2017/01/20/00/30/rose-1997282_1280.jpg";
-      } else if (flower === "sunflower") {
-        img.src = "https://cdn.pixabay.com/photo/2016/03/05/19/02/sunflower-1235721_1280.jpg";
-      } else if (flower === "lotus") {
-        img.src = "https://cdn.pixabay.com/photo/2017/01/20/00/30/lotus-1997280_1280.jpg";
-      }
+    
+    // function fun2(){
+    //   document.getElementById('image').src="tabletjpg"
+    // }
 
-      img.alt = flower + " image";
-    }
+    
+    // function fun3(){
+    //   document.getElementById('image').src="laptop.jpeg"
+    // }
+
+
+// --------------------------------------------------------------------------------
+
+// array denoted by []
+// array means many multiple variable or data we can store
+
+
+// {/* <h1>welcome</h1> */}
+
+// let h = document.getElementsByTagName('h1')
+// console.log(h[0])
+// h[0].style.color="red"
+
+
+//everything in dom is node==========================================
+
+// console.log(h[0])
+// let a = document.getElementById('h2').style.color="red"
+// let b= document.getElementById('h2').style.color="blue"
+// let c = document.getElementsByTagName('h2').style.color="black"
+// let d = document.getElementsByTagName('h2').style.color="green"       //doo
+// let e= document.getElementsByTagName('h2').style.color="yellow"
+// let f = document.getElementsByTagName('h2').style.color="pink"
+
+
+
+
+
+
+
+let z=document.getElementsByClassName('a')
+console.log(z[0])
+z[0].style.color="red"
+z[1].style.color="blue"
+z[2].style.color="green"
+z[3].style.color="yellow"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function showFlower(flower) {
+    //   let img = document.getElementById("flowerImage");
+
+    //   if (flower === "rose") {
+    //     img.src = "rese.jpg";
+    //   } else if (flower === "sunflower") {
+    //     img.src = "https://cdn.pixabay.com/photo/2016/03/05/19/02/sunflower-1235721_1280.jpg";
+    //   } else if (flower === "lotus") {
+    //     img.src = "https://cdn.pixabay.com/photo/2017/01/20/00/30/lotus-1997280_1280.jpg";
+    //   }
+
+    //   img.alt = flower + " image";
+    // }
