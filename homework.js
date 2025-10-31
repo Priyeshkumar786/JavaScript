cefdb06d7264f03d93dd0f4bce73202755697ead
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-10-31 21:17:55 +0530

    js side homework

--- homework.js
+++ homework.js
@@ -167,3 +167,6 @@
 // password must have special characters,one number and one capital and must no be less than 8 and greater than 15
 // ---------------------------------------------------------------------------------------------
 
+function validatePassword() {
+      let password = document.getElementById("password").value;
+      let result = document.getElementById("result");}
