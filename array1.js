e3218609e3a273136aafe344a6a8ba88074a6def
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-11-03 16:16:50 +0530

    added todays inbuilt function of array

---
+++ array1.js
@@ -0,0 +1,49 @@
+
+    // let ar = ["tanmay",34,"bhopal","india"]
+
+    // for(let vl of ar){
+    //     console.log(vl)
+    // }
+    // //25 to 1 print array access all even numbers and square them using for of loop
+   
+
+    // //for of is to be accesed in array
+
+    // let arr = [25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,7,6,5,4,3,2,1,0]
+    
+    // for(let even of arr){
+
+    //     if (even%2==0){
+    //         console.log(even*even)
+
+    //     }
+    // }
+
+    // // for in is to be used in object 
+
+    // for(let k in arr){
+    //     console.log(k);
+    // }
+
+    // in built funtions of array 
+
+    let ar = ['ajay','bhopal',23]
+
+    //push()                             it works like append adds one value at last
+    ar.push("js",'react')
+    console.log(ar)
+ 
+    //pop()                             removes last value in an array
+
+    ar.pop()
+    console.log(ar)
+
+    //unshift()                        adds one value at first position in array
+
+    ar.unshift("c","jas")
+    console.log(ar)
+
+    //shift()                          removes first value element of any array
+
+    ar.shift()
+    console.log(ar)
