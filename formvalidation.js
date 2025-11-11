5c79feac0f5948ecc718de045aeba9d3a341c888
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-11-11 14:38:11 +0530

    added morE

--- formvalidation.js
+++ formvalidation.js
@@ -1,4 +1,4 @@
 //formvalidation to be started--:
 
 // Form validation is the process of checking 
-// whether the information a user submits 
+// whether the information a user submits through a web form is accurate 
