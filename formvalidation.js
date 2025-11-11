82cfac2fc9c04fd094486c0aee8e2f7c10cc79ce
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-11-11 14:38:48 +0530

    added more

--- formvalidation.js
+++ formvalidation.js
@@ -2,3 +2,5 @@
 
 // Form validation is the process of checking 
 // whether the information a user submits through a web form is accurate 
+// and meets specific criteria
+// and meets specific criteria
