02d666181ebd9ca7c67bd8f59a720da6d768cd22
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-08 00:02:51 +0530

    added all

--- addevent.js
+++ addevent.js
@@ -166,3 +166,19 @@
 // })
 
 
+//function of increment , decrement and reset
+// var a = 0
+
+// document.querySelector('#add').addEventListener('click',function(){
+//       document.querySelector('#counter').innerHTML = ++a
+// })
+
+// document.querySelector('#sub').addEventListener('click',function(){
+//       document.querySelector('#counter').innerHTML = --a
+// })
+
+// document.querySelector('#reset').addEventListener('click',function(){
+//     document.querySelector("#counter").innerHTML = a=0
+// })
+
+
