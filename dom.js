81a053925e5b20b980c33b0d801698f2b502372d
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-25 22:22:10 +0530

    added another

--- dom.js
+++ dom.js
@@ -497,3 +497,18 @@
 
 // a = { b: "c" };
 // typeof a;				// "object"
+
+// ---------------------------------------------------------
+
+// var arr = [
+// 	"hello world",
+// 	42,
+// 	true
+// ];
+
+// arr[0];			// "hello world"
+// arr[1];			// 42
+// arr[2];			// true
+// arr.length;		// 3
+
+// typeof arr;		// "object"
