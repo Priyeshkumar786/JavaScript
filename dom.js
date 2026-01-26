26ea099a5828c8f5592cd151cea5c3ae11048a50
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-26 07:36:56 +0530

    practice question3

--- dom.js
+++ dom.js
@@ -554,3 +554,8 @@ let x = 10;
 
 console.log(y);            // undefined (var is initialized at hoist time)
 var y = 10;
+// -------------------------------------
+
+const arr = [1, 2, 3];
+arr[10] = 99;
+console.log(arr.length);
