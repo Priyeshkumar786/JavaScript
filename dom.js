12758fca028e83d42f9ebed78e01180adf9b20d1
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-26 07:36:01 +0530

    practice question1

--- dom.js
+++ dom.js
@@ -515,15 +515,33 @@
 
 // ---------------------------------------------------
 
-var obj = {
-	a: "hello world", // property
-	b: 42,
-	c: true
-};
-obj.a;		// "hello world", accessed with doted notation
-obj.b;		// 42
-obj.c;		// true
+// var obj = {
+// 	a: "hello world", // property
+// 	b: 42,
+// 	c: true
+// };
+// obj.a;		// "hello world", accessed with doted notation
+// obj.b;		// 42
+// obj.c;		// true
 
-obj["a"];	// "hello world", accessed with bracket notation
-obj["b"];	// 42
-obj["c"];	// true
+// obj["a"];	// "hello world", accessed with bracket notation
+// obj["b"];	// 42
+// obj["c"];	// true
+
+// ---------------------------------------------
+
+// var: function or global scope, can be re-declared and updated
+var x = 10;
+var x = 20; // re-declaration allowed
+console.log("var:", x); // 20
+
+// let: block scope, can be updated but not re-declared in same block
+let y = 30;
+// let y = 40; Error (can't re-declare in same block)
+y = 40; // update allowed
+console.log("let:", y); // 40
+
+// const: block scope, cannot be reassigned
+const z = 50;
+// z = 60; Error (can't reassign)
+console.log("const:", z); // 50
